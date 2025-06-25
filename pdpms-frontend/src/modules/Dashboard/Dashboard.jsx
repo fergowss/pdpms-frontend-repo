65272df3b113c6d105b61b0be7e5a18bf7a876cf
fergows <herson.arcangel.salvador21@gmail.com>
2025-06-25 11:36:09 +0800

    sample pull only

--- pdpms-frontend/src/modules/Dashboard/Dashboard.jsx
+++ pdpms-frontend/src/modules/Dashboard/Dashboard.jsx
@@ -4,9 +4,7 @@ export default function Dashboard() {
   return (
     <div className="Dashboard-Container">
       <h2 className="Dashboard-Title">Dashboard</h2>
-      <p>Placeholder content – build your dashboard hereeeeeee.</p>
-            <p>Placeholder content – build your dashboard hereeeeeee.</p>
-                  <p>Placeholder content – build your dashboard hereeeeeee.</p>
+      <p>Placeholder content – build your dashboard here.</p>
     </div>
   );
 }
