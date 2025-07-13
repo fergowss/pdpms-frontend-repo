91fc071262abaacad45f0cefeee461d78d8c87b4
wkwk08 <jerichoambrocio123@gmail.com>
2025-07-13 18:38:58 +0800

    revert(AddEmployeeModal): undo message hiding, debug logs, & visual feedback

--- pdpms-frontend/src/modules/Settings/submodules/AddEmployeeModal.jsx
+++ pdpms-frontend/src/modules/Settings/submodules/AddEmployeeModal.jsx
@@ -76,19 +76,6 @@ export default function AddEmployeeModal({ open, onClose, onAdd }) {
       }
     } else {
       setForm(prev => ({ ...prev, [name]: value }));
-      // Clear employee ID message when user starts typing in another field
-      if (employeeIdStatus === 'valid') {
-        setEmployeeIdStatus('');
-        setEmployeeIdMessage('');
-      }
-    }
-  };
-
-  const handleEmployeeIdBlur = () => {
-    // Clear the "Employee ID is available" message on blur if valid
-    if (employeeIdStatus === 'valid') {
-      setEmployeeIdStatus('');
-      setEmployeeIdMessage('');
     }
   };
 
@@ -132,7 +119,6 @@ export default function AddEmployeeModal({ open, onClose, onAdd }) {
                 name="employeeIdSuffix"
                 value={fullEmployeeId}
                 onChange={handleChange}
-                onBlur={handleEmployeeIdBlur}
                 maxLength={PREFIX.length + 4}
                 required
               />
